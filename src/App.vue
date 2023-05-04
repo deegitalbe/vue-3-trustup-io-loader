cfdff4b61bf12509d4b2096be79b408a5f182191
AM_Trustup <axelismv4630@gmail.com>
2023-05-04 15:34:53 +0200

    :sparkles: New options on component (size and colors)

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,12 @@
   <div
     class="h-screen w-screen flex items-center justify-center bg-gradient-to-br to-cyan-600 from-blue-500"
   >
-    <AppLoader />
+    <AppLoader
+      :is-full-page="true"
+      first-circle-color="#5980e3"
+      second-circle-color="#a6baed"
+      third-circle-color="#1B4CCA"
+    />
   </div>
 </template>
 
--- src/lib/components/AppLoader.vue
+++ src/lib/components/AppLoader.vue
@@ -1,6 +1,7 @@
 <template>
   <div
-    class="flex justify-center items-center bg-white h-screen fixed top-0 left-0 w-screen"
+    class="flex justify-center items-center bg-white"
+    :class="isFullPage ? 'fixed top-0 left-0 w-screen h-screen' : 'w-full'"
   >
     <div class="w-32">
       <svg
@@ -16,7 +17,7 @@
         xml:space="preserve"
       >
         <path
-          fill="#5980e3"
+          :fill="firstCircleColor === undefined ? '#5980e3' : firstCircleColor"
           d="M31.6,3.5C5.9,13.6-6.6,42.7,3.5,68.4c10.1,25.7,39.2,38.3,64.9,28.1l-3.1-7.9c-21.3,8.4-45.4-2-53.8-23.3
   c-8.4-21.3,2-45.4,23.3-53.8L31.6,3.5z"
         >
@@ -31,7 +32,7 @@
           />
         </path>
         <path
-          fill="#1B4CCA"
+          :fill="thirdCircleColor === undefined ? '#1B4CCA' : thirdCircleColor"
           d="M42.3,39.6c5.7-4.3,13.9-3.1,18.1,2.7c4.3,5.7,3.1,13.9-2.7,18.1l4.1,5.5c8.8-6.5,10.6-19,4.1-27.7
   c-6.5-8.8-19-10.6-27.7-4.1L42.3,39.6z"
         >
@@ -46,7 +47,9 @@
           />
         </path>
         <path
-          fill="#a6baed"
+          :fill="
+            secondCircleColor === undefined ? '#a6baed' : secondCircleColor
+          "
           d="M82,35.7C74.1,18,53.4,10.1,35.7,18S10.1,46.6,18,64.3l7.6-3.4c-6-13.5,0-29.3,13.5-35.3s29.3,0,35.3,13.5
   L82,35.7z"
         >
@@ -67,3 +70,14 @@
     </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  isFullPage: boolean;
+  firstCircleColor: string;
+  secondCircleColor: string;
+  thirdCircleColor: string;
+}>();
+
+console.log(props);
+</script>
